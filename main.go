c16c5fa1b50f0a2c1ed47c44a22dc16a7b1321c4
Themba <thembangubeni04@gmail.com>
2025-02-20 14:05:57 +0200

    Planning: Around with query builder structure

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"server/bootstrap"
+	query "server/query_test"
 )
 
 func main() {
 	server := bootstrap.Boot()
+
+	query.Test()
 
 	fmt.Printf("Server Running %s", server.Host())
 
