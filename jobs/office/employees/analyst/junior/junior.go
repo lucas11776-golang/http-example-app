8ed32c0198125abc2e863ba57e497d4fa2c7ec00
lucas11776 <lucas11776>
2025-06-05 16:58:51 +0200

    Updated: remove commit junior

--- jobs/office/employees/analyst/junior/junior.go
+++ jobs/office/employees/analyst/junior/junior.go
@@ -3,14 +3,14 @@ package junior
 import (
 	"context"
 	"encoding/json"
-	"io"
-	"os"
+	"server/env"
 	"server/jobs/office/utils"
 	"server/jobs/workspace"
 	"server/jobs/workspace/paperwork/analyst"
 	"time"
 
 	"github.com/lucas11776-golang/orm"
+	"google.golang.org/genai"
 )
 
 type JuniorAnalyst struct {
@@ -61,74 +61,68 @@ func (ctx *JuniorAnalyst) createArticleCapture(capture map[string]interface{}) (
 
 // Comment
 func (ctx *JuniorAnalyst) ResearchArticles(context context.Context, intrest []string) ([]*analyst.ArticleCapture, error) {
-	// client, err := genai.NewClient(context, &genai.ClientConfig{
-	// 	APIKey:  env.Env("AI_KEY_AI"),
-	// 	Backend: genai.BackendGeminiAPI,
-	// })
+	client, err := genai.NewClient(context, &genai.ClientConfig{
+		APIKey:  env.Env("AI_KEY_AI"),
+		Backend: genai.BackendGeminiAPI,
+	})
 
-	// if err != nil {
-	// 	return nil, err
-	// }
+	if err != nil {
+		return nil, err
+	}
 
-	// ask := `
-	// You are an analyst working for a company that analyses the web for news based on client descriptions.
-	// Your job is to find those news articles on the web, and they will be submitted to your senior analyst for review and approval.
-	// Remember the client depends on those news articles for their daily operations.
-	// Below are are bullet points of what the client wants:
+	ask := `
+	You are an analyst working for a company that analyses the web for news based on client descriptions.
+	Your job is to find those news articles on the web, and they will be submitted to your senior analyst for review and approval.
+	Remember the client depends on those news articles for their daily operations.
+	Below are are bullet points of what the client wants:
 
-	// - News must the the lastest current date is 05 June 2025.
-	// - News must be in South Africa.
-	// - Use news site from South Africa.
-	// - Get atleast 50 article but if the are not that intresting exclude them.
+	- News must the the lastest current date is 05 June 2025.
+	- News must be in South Africa.
+	- Use news site from South Africa.
+	- Get atleast 50 article but if the are not that intresting exclude them.
 
-	// After you are done analyzing the news article data please format the articles in JSON object in array containing the following interface and
-	// place the data inside <result><result> also do not include ` + "```json ``` in results." + `
+	After you are done analyzing the news article data please format the articles in JSON object in array containing the following interface and
+	place the data inside <result><result> also do not include ` + "```json ``` in results." + `
 
-	// interface Article {
-	// 	title: string;        // Article tile.
-	// 	category: string;     // Article category pick on based on article - (General,Business,Politics,Science,Health,Entertainment,Sport,Technology,Finance)
-	// 	website: string;      // Article url/source only website host.
-	// 	description: string;  // Short description of article.
-	// 	image: string;        // Article image please do not make up one if you can not find it leave it empty.
-	// 	publisher: string;    // Article publisher
-	// 	published_at: string; // Article published at format YYYY-DD-MM.
-	// 	content: string;      // Article content (must be text).
-	// }`
+	interface Article {
+		title: string;        // Article tile.
+		category: string;     // Article category pick on based on article - (General,Business,Politics,Science,Health,Entertainment,Sport,Technology,Finance)
+		website: string;      // Article url/source only website host.
+		description: string;  // Short description of article.
+		image: string;        // Article image please do not make up one if you can not find it leave it empty.
+		publisher: string;    // Article publisher
+		published_at: string; // Article published at format YYYY-DD-MM.
+		content: string;      // Article content (must be text).
+	}`
 
-	// content := []*genai.Content{
-	// 	{
-	// 		Parts: []*genai.Part{
-	// 			{Text: ask},
-	// 		},
-	// 		Role: genai.RoleUser,
-	// 	},
-	// }
+	content := []*genai.Content{
+		{
+			Parts: []*genai.Part{
+				{Text: ask},
+			},
+			Role: genai.RoleUser,
+		},
+	}
 
-	// response, err := client.Models.GenerateContent(context, env.Env("AI_MODEL"), content, &genai.GenerateContentConfig{
-	// 	ThinkingConfig: &genai.ThinkingConfig{
-	// 		IncludeThoughts: true,
-	// 	},
-	// 	Tools: []*genai.Tool{
-	// 		{
-	// 			GoogleSearch: &genai.GoogleSearch{},
-	// 		},
-	// 		{
-	// 			URLContext: &genai.URLContext{},
-	// 		},
-	// 	},
-	// })
+	response, err := client.Models.GenerateContent(context, env.Env("AI_MODEL"), content, &genai.GenerateContentConfig{
+		ThinkingConfig: &genai.ThinkingConfig{
+			IncludeThoughts: true,
+		},
+		Tools: []*genai.Tool{
+			{
+				GoogleSearch: &genai.GoogleSearch{},
+			},
+			{
+				URLContext: &genai.URLContext{},
+			},
+		},
+	})
 
-	// if err != nil {
-	// 	return nil, err
-	// }
+	if err != nil {
+		return nil, err
+	}
 
-	// result := utils.PAPERWORK_RESULT_REGEX.FindStringSubmatch(response.Text())
-
-	file, _ := os.Open("cache.txt")
-
-	response, _ := io.ReadAll(file)
-
-	result := utils.ResultFromPaperword(string(response))
+	result := utils.ResultFromPaperword(string(response.Text()))
 
 	if result == "" {
 		return []*analyst.ArticleCapture{}, nil
